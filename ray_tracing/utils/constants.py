36e92727b223acee3eec0b88f8e428a6c0ff6cf8
hieule <hieult1996@gmail.com>
2023-04-24 18:48:08 -0500

    add white, black, origin

--- ray_tracing/utils/constants.py
+++ ray_tracing/utils/constants.py
@@ -1,2 +1,13 @@
+import os
+import sys
+
+package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, package_path)
+
+import ray_tracing.elements.tuples as tuples
+
 EPSILON = 1e-6
 OBJECT_COUNT = 0
+BLACK = tuples.Color(0, 0, 0)
+WHITE = tuples.Color(1, 1, 1)
+ORIGIN = tuples.Point(0, 0, 0)
