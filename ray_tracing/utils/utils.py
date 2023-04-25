081d8f6abd2aa6e7c14e183549b00b953880339b
hieule <hieult1996@gmail.com>
2023-04-25 13:06:15 -0500

    context table to material and transformation

--- ray_tracing/utils/utils.py
+++ ray_tracing/utils/utils.py
@@ -1,5 +1,14 @@
+import os
+import sys
+
+package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, package_path)
+
 import math
 from .constants import *
+import ray_tracing.elements.tuples as tuples
+import ray_tracing.elements.materials as materials
+import ray_tracing.elements.matrix as matrix
 
 
 def equal(a, b):
@@ -31,3 +40,80 @@ def generate_uuid():
     global OBJECT_COUNT
     OBJECT_COUNT += 1
     return OBJECT_COUNT
+
+
+def context_table_to_list(context):
+    """
+    Converts a context table to a list.
+    """
+    given_list = []
+    given_list.append([element for element in context.table.headings])
+    for row in context.table.rows:
+        given_list.append([element for element in row])
+    return given_list
+
+
+def list_to_material(given_list):
+    """
+    Converts a list to a material.
+    """
+
+    material_properties = {}
+
+    for row in given_list:
+        if row[0].lower().find("material") != -1:
+            if row[0].lower().find("color") != -1:
+                color = row[1].strip("() ").split(",")
+                material_properties["color"] = tuples.Color(
+                    float(color[0]), float(color[1]), float(color[2])
+                )
+            elif row[0].lower().find("ambient") != -1:
+                material_properties["ambient"] = float(row[1])
+            elif row[0].lower().find("diffuse") != -1:
+                material_properties["diffuse"] = float(row[1])
+            elif row[0].lower().find("specular") != -1:
+                material_properties["specular"] = float(row[1])
+            elif row[0].lower().find("shininess") != -1:
+                material_properties["shininess"] = float(row[1])
+    if material_properties == {}:
+        material = materials.Material()
+    else:
+        material = materials.Material(**material_properties)
+    return material
+
+
+def list_to_transform(given_list):
+    """
+    Converts a list to a transform.
+    """
+    transform = matrix.IdentityMatrix()
+    for row in given_list:
+        if row[0].lower().find("transform") != -1:
+            [operation, values] = row[1].strip(")").split("(")
+            values = values.split(",")
+            if operation.lower().find("scaling") != -1:
+                transform = transform.scale(
+                    float(values[0]), float(values[1]), float(values[2])
+                )
+            elif operation.lower().find("translation") != -1:
+                transform = transform.translate(
+                    float(values[0]), float(values[1]), float(values[2])
+                )
+            elif operation.lower().find("rotation_x") != -1:
+                transform = transform.rotate_x(float(values[0]))
+            elif operation.lower().find("rotation_y") != -1:
+                transform = transform.rotate_y(float(values[0]))
+            elif operation.lower().find("rotation_z") != -1:
+                transform = transform.rotate_z(float(values[0]))
+            elif operation.lower().find("shearing") != -1:
+                operation = matrix.ShearingMatrix(
+                    float(values[0]),
+                    float(values[1]),
+                    float(values[2]),
+                    float(values[3]),
+                    float(values[4]),
+                    float(values[5]),
+                )
+                transform = operation * transform
+
+    return transform
